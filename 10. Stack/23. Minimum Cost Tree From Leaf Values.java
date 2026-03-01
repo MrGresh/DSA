f50d5c118d7feff3c056a047a2cd52effce51ada
Mr. Gresh <greshseh@gmail.com>
2026-03-01 15:39:19 +0530

    Create 23. Minimum Cost Tree From Leaf Values.java

---
+++ 10. Stack/23. Minimum Cost Tree From Leaf Values.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        stack.push(Integer.MAX_VALUE);
+
+        for (int num : arr) {
+            while (num >= stack.peek()) {
+                result += stack.pop() * Math.min(stack.peek(), num);
+            }
+            stack.push(num);
+        }
+
+        while (stack.size() > 2) {
+            result += stack.pop() * stack.peek();
+        }
+
+        return result;
+    }
+}
